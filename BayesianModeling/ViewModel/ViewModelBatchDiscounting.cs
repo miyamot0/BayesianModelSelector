14fb72d4cc2180e0c994ebfaa9f9658275744d51
Shawn Gilroy <ShawnPGilroy@gmail.com>
2016-05-25 01:22:55 -0400

    Prevent unnecesary evaluations in batches, minor speedup

--- BayesianModeling/ViewModel/ViewModelBatchDiscounting.cs
+++ BayesianModeling/ViewModel/ViewModelBatchDiscounting.cs
@@ -843,14 +843,14 @@ namespace BayesianModeling.ViewModel
 
                     engine.Evaluate("datHack<-data.frame(X = mDelays, Y = mIndiffs, ses=mSes)");
                     engine.Evaluate("output <- BDS(datHack)");
-                    engine.Evaluate("ainslieK <- as.numeric(output[[2]]['Mazur.lnk'])");
-                    engine.Evaluate("samuelsonK <- as.numeric(output[[3]]['exp.lnk'])");
-                    engine.Evaluate("beta <- as.numeric(output[[9]]['BD.beta'])");
-                    engine.Evaluate("delta <- as.numeric(output[[9]]['BD.delta'])");
-                    engine.Evaluate("myersonK <- as.numeric(output[[4]]['MG.lnk'])");
-                    engine.Evaluate("myersonS <- as.numeric(output[[4]]['MG.s'])");
-                    engine.Evaluate("rachlinK <- as.numeric(output[[5]]['Rachlin.lnk'])");
-                    engine.Evaluate("rachlinS <- as.numeric(output[[5]]['Rachlin.s'])");
+                    //engine.Evaluate("ainslieK <- as.numeric(output[[2]]['Mazur.lnk'])");
+                    //engine.Evaluate("samuelsonK <- as.numeric(output[[3]]['exp.lnk'])");
+                    //engine.Evaluate("beta <- as.numeric(output[[9]]['BD.beta'])");
+                    //engine.Evaluate("delta <- as.numeric(output[[9]]['BD.delta'])");
+                    //engine.Evaluate("myersonK <- as.numeric(output[[4]]['MG.lnk'])");
+                    //engine.Evaluate("myersonS <- as.numeric(output[[4]]['MG.s'])");
+                    //engine.Evaluate("rachlinK <- as.numeric(output[[5]]['Rachlin.lnk'])");
+                    //engine.Evaluate("rachlinS <- as.numeric(output[[5]]['Rachlin.s'])");
 
                     double noiseProb = double.Parse(engine.Evaluate("as.numeric(output[[1]]['noise.prob'])").AsVector().First().ToString(), System.Globalization.NumberStyles.Float);
                     double hyperProb = double.Parse(engine.Evaluate("as.numeric(output[[2]]['Mazur.prob'])").AsVector().First().ToString(), System.Globalization.NumberStyles.Float);
