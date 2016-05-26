12d45f3f917d066f3ee5393e043a34b7af2ec475
Shawn Gilroy <ShawnPGilroy@gmail.com>
2016-05-25 21:55:14 -0400

    Revert remembered window location, default center current screen

--- BayesianModeling/Properties/Settings.Designer.cs
+++ BayesianModeling/Properties/Settings.Designer.cs
@@ -61,30 +61,6 @@ namespace BayesianModeling.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
-        public double MainTop {
-            get {
-                return ((double)(this["MainTop"]));
-            }
-            set {
-                this["MainTop"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
-        public double MainLeft {
-            get {
-                return ((double)(this["MainLeft"]));
-            }
-            set {
-                this["MainLeft"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("400")]
         public double ResultWidth {
             get {
@@ -109,54 +85,6 @@ namespace BayesianModeling.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
-        public double ResultTop {
-            get {
-                return ((double)(this["ResultTop"]));
-            }
-            set {
-                this["ResultTop"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
-        public double ResultLeft {
-            get {
-                return ((double)(this["ResultLeft"]));
-            }
-            set {
-                this["ResultLeft"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
-        public double DiscountTop {
-            get {
-                return ((double)(this["DiscountTop"]));
-            }
-            set {
-                this["DiscountTop"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
-        public double DiscountLeft {
-            get {
-                return ((double)(this["DiscountLeft"]));
-            }
-            set {
-                this["DiscountLeft"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool Updated {
             get {
@@ -164,30 +92,6 @@ namespace BayesianModeling.Properties {
             }
             set {
                 this["Updated"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
-        public double BatchDiscountTop {
-            get {
-                return ((double)(this["BatchDiscountTop"]));
-            }
-            set {
-                this["BatchDiscountTop"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
-        public double BatchDiscountLeft {
-            get {
-                return ((double)(this["BatchDiscountLeft"]));
-            }
-            set {
-                this["BatchDiscountLeft"] = value;
             }
         }
         
