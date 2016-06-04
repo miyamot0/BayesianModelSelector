1fa236aa72fc598b48158d48399018f6aa3467bd
Shawn Gilroy <ShawnPGilroy@gmail.com>
2016-06-04 09:32:56 -0400

    More licensing standardization

--- BayesianModeling/View/ImageWindow.xaml.cs
+++ BayesianModeling/View/ImageWindow.xaml.cs
@@ -1,4 +1,31 @@
-﻿using Microsoft.Win32;
+﻿//----------------------------------------------------------------------------------------------
+// <copyright file="ImageWindow.cs" 
+// Copyright 2016 Shawn Gilroy
+//
+// This file is part of Bayesian Model Selector.
+//
+// Bayesian Model Selector is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2.
+//
+// Bayesian Model Selector is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Bayesian Model Selector.  If not, see http://www.gnu.org/licenses/. 
+// </copyright>
+//
+// <summary>
+// The Bayesian Model Selector is a tool to assist researchers in behavior economics.
+// 
+// Email: shawn(dot)gilroy(at)temple.edu
+//
+// </summary>
+//----------------------------------------------------------------------------------------------
+
+using Microsoft.Win32;
 using System.IO;
 using System.Windows;
 using System.Windows.Media;
--- BayesianModeling/View/MainWindow.xaml.cs
+++ BayesianModeling/View/MainWindow.xaml.cs
@@ -1,21 +1,29 @@
-﻿/* 
-    Copyright 2016 Shawn Gilroy
-
-    This file is part of Bayesian Model Selector.
-
-    Bayesian Model Selector is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, version 2.
-
-    Bayesian Model Selector is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Bayesian Model Selector.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
-
- */
+﻿//----------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.cs" 
+// Copyright 2016 Shawn Gilroy
+//
+// This file is part of Bayesian Model Selector.
+//
+// Bayesian Model Selector is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2.
+//
+// Bayesian Model Selector is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Bayesian Model Selector.  If not, see http://www.gnu.org/licenses/. 
+// </copyright>
+//
+// <summary>
+// The Bayesian Model Selector is a tool to assist researchers in behavior economics.
+// 
+// Email: shawn(dot)gilroy(at)temple.edu
+//
+// </summary>
+//----------------------------------------------------------------------------------------------
 
 using BayesianModeling.ViewModel;
 using Microsoft.Win32;
